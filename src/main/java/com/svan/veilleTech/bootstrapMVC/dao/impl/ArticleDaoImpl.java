6649dbc3e9723b9504bc15cc09020911c0133ec7
stephane <stephane.gronowski@gmail.com>
2014-09-06 22:19:35 +0200

    Complete dao

--- src/main/java/com/svan/veilleTech/bootstrapMVC/dao/impl/ArticleDaoImpl.java
+++ src/main/java/com/svan/veilleTech/bootstrapMVC/dao/impl/ArticleDaoImpl.java
@@ -4,6 +4,8 @@
 package com.svan.veilleTech.bootstrapMVC.dao.impl;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import org.springframework.stereotype.Repository;
 
@@ -15,21 +17,26 @@ import com.svan.veilleTech.bootstrapMVC.domain.Article;
  * Default Implementaion of {@link ArticleDao}
  * 
  * @author stephane 3 sept. 2014
- *
+ * 
  */
 @Repository
-public class ArticleDaoImpl extends AbstractFwkDao<Article, Long> implements
-		ArticleDao {
+public class ArticleDaoImpl extends AbstractFwkDao<Article, Long> implements ArticleDao {
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.svan.fwk.dao.AbstractFwkDao#getEntityManager()
-	 */
+	@PersistenceContext(unitName = "bootstrap-pu")
+	private EntityManager entityManager;
+
 	@Override
 	protected EntityManager getEntityManager() {
-		// TODO Auto-generated method stub
-		return null;
+		return entityManager;
 	}
+	
+	
+	public Article getGenerationBy(String documentId) {
+		TypedQuery<Article> query = entityManager.createNamedQuery(
+				"", Article.class);
 
+		query.setParameter("documentId", documentId);
+
+		return singleResultOrNull(query);
+	}
 }
