d03cc528967bfbf7ae3ea3fbb015d4d781af1b88
stephane <stephane.gronowski@gmail.com>
2014-11-08 00:30:34 +0100

    clean

--- src/main/java/com/svan/veilleTech/bootstrapMVC/controller/MainController.java
+++ src/main/java/com/svan/veilleTech/bootstrapMVC/controller/MainController.java
@@ -1,23 +1,67 @@
+/**
+ * 2014
+ * 17 oct. 2014 23:37:42 
+ * @author stephane stephane.gronowski@gmail.com
+ */
 package com.svan.veilleTech.bootstrapMVC.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.svan.veilleTech.bootstrapMVC.dto.NewsDTO;
+import com.svan.veilleTech.bootstrapMVC.service.NewsService;
+
+/**
+ * 
+ * 17 oct. 2014 23:37:42
+ * 
+ * @author stephane stephane.gronowski@gmail.com
+ * 
+ */
 @Controller
-@RequestMapping(value="/main")
 public class MainController {
 
-	@RequestMapping(value = "/test", method = RequestMethod.GET)
-	public ModelAndView index() {
-		return new ModelAndView("index");
+	@Autowired
+	private NewsService newsService;
+
+	@RequestMapping(value = "index")
+	public String index() {
+		return "index";
 	}
 
-	@RequestMapping(value = "/bootstrap", method = RequestMethod.GET)
-	public ModelAndView bootstrap() {
-		return new ModelAndView("bootstrap");
+	@RequestMapping(value = "news")
+	public ModelAndView news() {
+		ModelAndView mav = new ModelAndView("news");
+
+		List<NewsDTO> news = newsService.getLastNews();
+		mav.addObject("listNews", news);
+
+		return mav;
 	}
 
-	
+	@RequestMapping(value = "members")
+	public ModelAndView members() {
+		ModelAndView mav = new ModelAndView("members");
+
+		return mav;
+	}
+
+	@RequestMapping(value = "galerie")
+	public ModelAndView galerie() {
+		ModelAndView mav = new ModelAndView("galerie");
+
+		return mav;
+	}
+
+	@RequestMapping(value = "recrutement")
+	public ModelAndView recrutement() {
+		ModelAndView mav = new ModelAndView("recrutement");
+
+		return mav;
+	}
+
 }
