a097ac38d46d4062f7b66123fc9b1c509a32220f
stephane <stephane.gronowski@gmail.com>
2014-12-26 15:37:19 +0100

    Exemple insert bdd

---
+++ Doc/exemple_insert.sql
@@ -0,0 +1,24 @@
+-- TEAM MEMBER
+insert into team_member 
+(nick_name, forname, sexe, role, birth_date, description, gear, status, likes, doesntlikes, membership_date, creation_date) 
+values ('soldier', 'Mathieu', 'M', 'Leader', sysdate(), 'Createur/leader/president de la team', 'T15, T8', 'L', 'Tiberius, ACIM, vert olive', '', sysdate(), sysdate());
+
+insert into team_member 
+(nick_name, forname, sexe, role, birth_date, description, gear, status, likes, doesntlikes, membership_date, creation_date) 
+values ('forest', 'Jerome', 'M', 'Medic', sysdate(), 'Medic de la team', 'RAM, SAR-12', 'M', 'Petit calibres, couteau, comics', '', sysdate(), sysdate());
+
+
+insert into team_member 
+(nick_name, forname, sexe, role, birth_date, description, gear, status, likes, doesntlikes, membership_date, creation_date) 
+values ('scarface', 'Sebastien', 'M', 'Operateur', sysdate(), 'Operateur de la team', 'T15, T8', 'M', 'Tiberius, \"grenade\"', '', sysdate(), sysdate());
+
+
+insert into team_member 
+(nick_name, forname, sexe, role, birth_date, description, gear, status, likes, doesntlikes, membership_date, creation_date) 
+values ('macdo', 'Reynald', 'M', 'Operateur', sysdate(), 'Operateur de la team', 'M17', 'P', 'Frite', '', sysdate(), sysdate());
+
+
+insert into team_member 
+(nick_name, forname, sexe, role, birth_date, description, gear, status, likes, doesntlikes, membership_date, creation_date) 
+values ('svan', 'Stephane', 'M', 'Operateur', '1988-10-18', 'Operateur de la team', 'TGR36, Tipx, T98', 'M', 'Musique/guitare, paint', 'apple', '2014-02-01', sysdate());
+
