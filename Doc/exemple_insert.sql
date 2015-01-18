8382d920da02f021a4c37ea87167d66aef23dae6
stephane <stephane.gronowski@gmail.com>
2015-01-18 18:51:40 +0100

    Ajout de velotr

--- Doc/exemple_insert.sql
+++ Doc/exemple_insert.sql
@@ -22,6 +22,11 @@ insert into team_member
 (nick_name, forname, sexe, role, birth_date, description, gear, status, likes, doesntlikes, membership_date, creation_date) 
 values ('svan', 'Stephane', 'M', 'Operateur', '1988-10-18', 'Operateur de la team', 'TGR36, Tipx, T98', 'M', 'Musique/guitare, paint', 'apple', '2014-02-01', sysdate());
 
+insert into team_member 
+(nick_name, forname, sexe, role, birth_date, description, gear, status, likes, doesntlikes, membership_date, creation_date) 
+values ('velotr', 'Jordan', 'M', 'Operateur', '1993-09-08', 'Opérateur', 'RAP468, crosman 3357', 'M', 'La bière', 'Le froid', '2013-11-08', sysdate());
+
+
 -- Gallery
 
 insert into gallery (title, creation_date) values ('Les Membres', '2015-01-01');
@@ -34,6 +39,7 @@ insert into picture (gallery, title, creation_date) values (1, 'macdo', '2015-01
 insert into picture (gallery, title, creation_date) values (1, 'forest', '2015-01-01');
 insert into picture (gallery, title, creation_date) values (1, 'scarface', '2015-01-01');
 insert into picture (gallery, title, creation_date) values (1, 'svan', '2015-01-01');
+insert into picture (gallery, title, creation_date) values (1, 'velotr', '2015-01-01');
 
 insert into picture (gallery, title, creation_date) values (2, 'pompe', '2015-01-01');
 insert into picture (gallery, title, creation_date) values (2, 'gun', '2015-01-01');
