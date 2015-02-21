cdb2e135e052490ce5f81cbc1d0f5133c3ab84c1
stephane <stephane.gronowski@gmail.com>
2015-02-21 16:02:09 +0100

    Issue #4 : [BUG] Le li du header n'est pas selectioner si recharge la page : utilise un service et ecoute les evenement de changement de vue

--- war/assets/app/navbar/navbarModule.js
+++ war/assets/app/navbar/navbarModule.js
@@ -4,18 +4,55 @@
 var navbarModule = angular.module('navbarModule', []);
 
 // Controller
-navbarModule.controller('navbarCtrl', [ '$scope',
-
-function($scope) {
-	$scope.selectedLi = 'home';
+navbarModule.controller('navbarCtrl', [ '$scope', 'navbarService',
+function($scope, navbarService) {
+	// Navbar collapse
 	$scope.isCollapsed = true;
 
+	/**Selectione un menu*/
 	$scope.selectLi = function(select) {
-		$scope.selectedLi = select;
 		// assure que le menue est collapse
 		$scope.isCollapsed = true;
-	}
+	};
 
-}// END CONTROLLER
+	$scope.isCurrentView = navbarService.isCurrentView;
+	
+}]);
+//END CONTROLLER
 
-]);
+// Service
+navbarModule.factory('navbarService', function() {
+	var currentView = 'home';
+	return {
+		getCurrentView : function(){
+			return currentView;
+		},
+		setCurrentView : function(newView){
+			currentView = newView;
+		},
+		isCurrentView : function(value) {
+			return currentView == value;
+		}
+	};
+});
+// END Service
+
+
+//block run pour gestion du menu a selectionner selon la route
+navbarModule.run(function($location, $rootScope, navbarService) {
+	$rootScope.$on('$routeChangeStart', function() {
+		
+		// Check commence par "/" + view
+		if ($location.path().indexOf('home') == 1) {
+			navbarService.setCurrentView('home');
+		} else if ($location.path().indexOf('news') == 1) {
+			navbarService.setCurrentView('news');
+		} else if ($location.path().indexOf('member') == 1) {
+			navbarService.setCurrentView('member');
+		} else if ($location.path().indexOf('gallery') == 1) {
+			navbarService.setCurrentView('gallery');
+		} else if ($location.path().indexOf('recrutement') == 1) {
+			navbarService.setCurrentView('recrutement');
+		} 
+	});
+});
