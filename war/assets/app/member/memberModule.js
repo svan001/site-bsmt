b3a2c6ed486a368db3fdcedeee42d3c3ba3ee0ab
stephane <stephane.gronowski@gmail.com>
2015-02-21 17:02:37 +0100

    Issue #13 : Ordonner les membres à l'affichage de la page membre Ordonne par : Leader => Membre/postulant => amis

--- war/assets/app/member/memberModule.js
+++ war/assets/app/member/memberModule.js
@@ -14,9 +14,20 @@ memberModule.factory('TeamMember', [ '$resource', function($resource) {
 memberModule.controller('memberCtrl', [ '$scope', '$resource', 'TeamMember',
 
 function($scope, $resource, TeamMember) {
-
+	// Init
 	$scope.teamMembers = TeamMember.query();
 
+	// Filter method : Leader => Membre/postulant => amis
+	$scope.statusFunctionFiler = function(item){
+		if(item.status == 'L'){
+			return -1;
+		} else if(item.status == 'M' || item.status == 'P'){
+			return 0;
+		} else if(item.status == 'A'){
+			return 1;
+		} else return 10;
+	};
+	
 }// END CONTROLLER
 ]);
 
