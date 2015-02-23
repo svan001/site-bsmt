187aaf5072cf21503cf010913826c6e46d7118ad
stephane <stephane.gronowski@gmail.com>
2015-02-23 23:57:18 +0100

    Ajout news paste bin

---
+++ Doc/pastebin sql.sql
@@ -0,0 +1,12 @@
+
+insert into news (author, title, creation_date, content) 
+values 
+( 'Svan', 'Rencontre LHRM', sysdate(), '
+<p>
+La black shark milsim team remercie la Horde Russe Milsim (LHRM) pour leur accueil dimanche sur leur terrain ! 
+</p>
+<p>
+Comme à chaque de bonne partie et une ambiance au top, au plaisir de se revori les gars ;)  
+</p>
+
+<img alt="Gallery1" src="https://fbcdn-sphotos-b-a.akamaihd.net/hphotos-ak-xfp1/v/t1.0-9/s720x720/11009098_815803475152109_5095126578511534066_n.jpg?oh=eae74a136711d0e87782c3044a7215b2&oe=5554C151&__gda__=1434584929_1d532e634973c5d232da70b96f9f5204" class="thumbnail img-rounded img-responsive" />');
