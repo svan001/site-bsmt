95232ab8bd531430b6a282d9e91175a6674def7c
stephane <stephane.gronowski@gmail.com>
2015-02-25 23:56:17 +0100

    Ajout galleri LHRM

--- Doc/pastebin sql.sql
+++ Doc/pastebin sql.sql
@@ -10,3 +10,28 @@ Comme à chaque de bonne partie et une ambiance au top, au plaisir de se revoir 
 </p>
 
 <img alt="Gallery1" src="https://fbcdn-sphotos-b-a.akamaihd.net/hphotos-ak-xfp1/v/t1.0-9/s720x720/11009098_815803475152109_5095126578511534066_n.jpg?oh=eae74a136711d0e87782c3044a7215b2&oe=5554C151&__gda__=1434584929_1d532e634973c5d232da70b96f9f5204" class="thumbnail img-rounded img-responsive" />');
+
+
+-- 25 12
+
+insert into gallery (title, creation_date) values ('LHRM 22/02/2015', sysdate());
+
+
+insert into picture (gallery, title, creation_date) values (3, '1', sysdate());
+insert into picture (gallery, title, creation_date) values (3, '2', sysdate());
+insert into picture (gallery, title, creation_date) values (3, '3', sysdate());
+insert into picture (gallery, title, creation_date) values (3, '4', sysdate());
+insert into picture (gallery, title, creation_date) values (3, '5', sysdate());
+insert into picture (gallery, title, creation_date) values (3, '6', sysdate());
+insert into picture (gallery, title, creation_date) values (3, '7', sysdate());
+insert into picture (gallery, title, creation_date) values (3, '8', sysdate());
+insert into picture (gallery, title, creation_date) values (3, '9', sysdate());
+insert into picture (gallery, title, creation_date) values (3, '10', sysdate());
+insert into picture (gallery, title, creation_date) values (3, 'all1', sysdate());
+insert into picture (gallery, title, creation_date) values (3, 'all2', sysdate());
+insert into picture (gallery, title, creation_date) values (3, 'kawa', sysdate());
+insert into picture (gallery, title, creation_date) values (3, 'svan', sysdate());
+insert into picture (gallery, title, creation_date) values (3, 'terro', sysdate());
+insert into picture (gallery, title, creation_date) values (3, 'velotr', sysdate());
+insert into picture (gallery, title, creation_date) values (3, 'zlatan', sysdate());
+insert into picture (gallery, title, creation_date) values (3, 'zlatan2', sysdate());
