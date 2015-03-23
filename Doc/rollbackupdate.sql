3f394040bb58837914745bf65c7f5ba8797ea2cf
stephane <stephane.gronowski@gmail.com>
2015-03-23 22:26:03 +0100

    rollback update foireux en prod

---
+++ Doc/rollbackupdate.sql
@@ -0,0 +1,72 @@
+-- NEWS
+insert into news (author, title, creation_date, content) 
+values 
+( 'Svan', 'Lancement du site !', '2015-03-19', '
+<p>
+La Black Shark Milsim Team lance officiellement son site ! Ce site a pour but d''être un moyen simple pour vous de mieux connaitre notre team ainsi que d''y trouver notre actualité.      
+</p>
+<p>
+En espérant qu''il vous plaira et vous y revoir régulièrement ;).  
+</p>
+
+');
+
+
+insert into news (author, title, creation_date, content) 
+values 
+( 'Svan', 'Training', '2015-03-01', '
+<p>
+En ce moment c''est training pour la BSMT, l''occasion de se remmetre les bons automatisme en tête.      
+</p>
+<p>
+On vous laisse avec une petite prise de vue de l''arsenal de la team, on approche du full magfeed :).  
+</p>
+<img alt="Arsenal" src="api/gallery/4/picture/magfeed" class="thumbnail img-rounded img-responsive" />
+');
+
+
+
+insert into news (author, title, creation_date, content) 
+values 
+( 'Svan', 'Rencontre LHRM', '2015-02-23', '
+<p>
+La Black Shark Milsim Team remercie la Horde Russe Milsim (LHRM) pour leur accueil dimanche sur leur terrain ! 
+</p>
+<p>
+Comme à chaque de bonne partie et une ambiance au top, au plaisir de se revoir les gars ;)  
+</p>
+
+<img alt="Gallery1" src="api/gallery/3/picture/all2" class="thumbnail img-rounded img-responsive" />');
+
+
+
+insert into news (author, title, creation_date, content) 
+values 
+( 'Cpt soldier', 'Journée à la grotte !', '2015-02-22', '
+<p>
+La black shark milsim team remercie les The UNIT MilsimTeam pour leur accueil aujourdhui à la grotte où nous avons passer une journée terrible, grand merci a eux !! 
+</p>
+<p>Merci aussi a toutes les teams présente : TASK FORCE 141, Toxico2 Paintball Milsim, LcDeux, FRAP Milsim, Eagle One Paintball Milsim, les spectres et zendoli grand plaisir d''avoir revue certain est d''en avoir rencontré d''autre.
+</p>
+<p>
+En espérant tous vous revoir bientot !
+</p>
+<img alt="Gallery1" src="https://fbcdn-sphotos-g-a.akamaihd.net/hphotos-ak-xpa1/v/t1.0-9/p480x480/10940623_978951058789191_1276044703747315962_n.jpg?oh=6fa99f236ea7c9bea7f9df895f15b3b1&oe=55485D56&__gda__=1434584496_919f9ac749594409d2e484004d74b759" class="thumbnail img-rounded img-responsive" />');
+
+
+insert into news (author, title, creation_date, content) 
+values 
+( 'Svan', 'Op Hiver Rude by Socom', '2015-01-11', '
+<p>
+Une petite news pour vous dire que la BMST, après avoir campée sur place toute la nuit et risquée la confrontation avec une chêvre visiblement énervée, a passée une super journée à l''OP Hiver Rude des Socom !      
+</p>
+<p>
+La bsmt remercie tous les participants à l''op hiver rude ainsi que les organisateurs la socom pour leur accueil et leurs efforts déployés pour que tous ce monde passe une bonne journée !!  
+</p>
+<p>
+En espérant recroiser prochainement les équipes présentes, on vous laisse avec cette image.
+</p>
+<img alt="coucou" src="api/gallery/5/picture/coucou" class="thumbnail img-rounded img-responsive" />
+');
+
+
