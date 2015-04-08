aed38bc5d345073c08c996ed14c8c3f7064a7b90
stephane <stephane.gronowski@gmail.com>
2015-04-08 23:15:01 +0200

    MAj site CPM

--- Doc/pastebin sql.sql
+++ Doc/pastebin sql.sql
@@ -60,3 +60,41 @@ Encore félicitation et à bientôt sur le terrain ma poule !
 </p>
 <img alt="macdo" src="api/gallery/1/picture/macdo_membre" class="thumbnail img-rounded" height="300"/>
 ');
+
+-- NEWS CPM
+
+
+insert into gallery (id, title, creation_date) values (6, 'OP Nuclear_Strike', sysdate());
+
+
+insert into picture (gallery, title, creation_date) values (6, 'bigsac', sysdate());
+insert into picture (gallery, title, creation_date) values (6, 'blc', sysdate());
+insert into picture (gallery, title, creation_date) values (6, 'bouclier', sysdate());
+insert into picture (gallery, title, creation_date) values (6, 'callof1', sysdate());
+insert into picture (gallery, title, creation_date) values (6, 'callof2', sysdate());
+insert into picture (gallery, title, creation_date) values (6, 'callof3', sysdate());
+insert into picture (gallery, title, creation_date) values (6, 'chill', sysdate());
+insert into picture (gallery, title, creation_date) values (6, 'cont', sysdate());
+insert into picture (gallery, title, creation_date) values (6, 'macdo', sysdate());
+insert into picture (gallery, title, creation_date) values (6, 'note', sysdate());
+insert into picture (gallery, title, creation_date) values (6, 'prep', sysdate());
+insert into picture (gallery, title, creation_date) values (6, 'soldier', sysdate());
+insert into picture (gallery, title, creation_date) values (6, 'tchetchne', sysdate());
+insert into picture (gallery, title, creation_date) values (6, 'together1', sysdate());
+insert into picture (gallery, title, creation_date) values (6, 'together2', sysdate());
+insert into picture (gallery, title, creation_date) values (6, 'together3', sysdate());
+
+
+insert into news (author, title, creation_date, content) 
+values 
+( 'Soldier', 'CPM : OP Nuclear Strike !', sysdate(), '
+<p>
+Nous remercions la CPM pour leur aceuil et leur invitation à leur OP Nuclear Strike ainsi que les teams presentes KUT / Task Force / TPT/ Socom / 6th Eagle / Ares. Un plaisir d''avoir revu certain et d''avoir rencontrer d''autre en espérant tous vous revoir sur le tarmak !!
+</p>
+<p>
+L''amusement et le bon temp etait de la partie et nous avons tous passé une bonne journée !! </p>
+<p>
+Un plaisir d''avoir vu de bonne progression de parte et d''autre, les photos de ce week end sont dispo dans la gallerie, merci à tous !!
+</p>
+<img alt="macdo" src="api/gallery/6/picture/together3" class="thumbnail img-rounded img-responsive"/>
+');
