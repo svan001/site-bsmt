58541f997368f92046430abbff2840a64c05227f
stephane <stephane.gronowski@gmai.com>
2015-07-31 13:08:10 +0200

    [TEMPLATE] Ajout de template de maj NEWS + MEMBER

---
+++ Doc/maj/template/AA_MM_DD_UPD_member_DESC.sql
@@ -0,0 +1,21 @@
+/**
+ * Script UPDATE Member : DESC
+ */
+
+-- Member
+UPDATE team_member 
+SET	
+	-- nick_name = '',
+	-- forname = '', 
+	-- sexe = '', -- M ou F
+	-- role = '', 
+	-- birth_date = '1988-10-18', 
+	-- description = '', 
+	-- gear = '', 
+	-- status = '', -- P / M / A / L
+	-- likes = '', 
+	-- doesntlikes = '', 
+	-- membership_date = '2015-01-30'
+	 
+WHERE nick_name = ''
+;
---
+++ Doc/maj/template/AA_MM_DD_UPD_news_DESC.sql
@@ -0,0 +1,14 @@
+/**
+ * Script UPDATE News : 
+ */
+
+--NEWS
+UPDATE news 
+SET 
+-- author = '',
+-- title = '', 
+-- creation_date = '', 
+-- content = '' 
+WHERE 
+	id = ;
+
