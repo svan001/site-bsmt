64321982611a370c968de268288eeee37dce6434
stephane <stephane.gronowski@gmail.com>
2016-01-03 19:49:11 +0100

    [#50] Importe les exceptions du FWK

---
+++ src/main/java/com/svan/veilleTech/bootstrapMVC/exception/NotImplementedYetException.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package com.svan.veilleTech.bootstrapMVC.exception;
+
+/**
+ * Method not implemented yet !
+ * 
+ * @author stephane 3 sept. 2014
+ * 
+ */
+public class NotImplementedYetException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7778560185562136049L;
+
+	/**
+	 * 
+	 */
+	public NotImplementedYetException() {
+	}
+
+	/**
+	 * @param message
+	 */
+	public NotImplementedYetException(String message) {
+		super(message);
+	}
+
+	/**
+	 * @param cause
+	 */
+	public NotImplementedYetException(Throwable cause) {
+		super(cause);
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public NotImplementedYetException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
