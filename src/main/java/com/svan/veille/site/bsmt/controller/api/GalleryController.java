c196419f2b5d8e56cde67cb6a2ee12a58abe6aec
stephane <stephane.gronowski@gmai.com>
2016-01-09 23:53:05 +0100

    [CLEAN] organize import

--- src/main/java/com/svan/veille/site/bsmt/controller/api/GalleryController.java
+++ src/main/java/com/svan/veille/site/bsmt/controller/api/GalleryController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.mvc.AbstractController;
 
 import com.svan.veille.site.bsmt.dto.GalleryDTO;
 import com.svan.veille.site.bsmt.service.GalleryService;
