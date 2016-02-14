25255e33c6b05df7c937acfb1d134004a9d8c628
stephane <stephane.gronowski@gmai.com>
2016-02-14 15:09:55 +0100

    [#62] Clean du module + finalisation du carousel de show gallery

--- war/assets/app/gallery/galleryModule.js
+++ war/assets/app/gallery/galleryModule.js
@@ -1,68 +1,77 @@
-'use strict';
+(function() {
 
-// MODULE
-var galleryModule = angular.module('galleryModule', [ 'ngResource' ]);
+	'use strict';
 
-// Resource Gallery
-galleryModule.factory('Gallery', [ '$resource', function($resource) {
-	return $resource('api/gallery/:id', {
-		id : '@id'
-	});
-} ]);
+	var galleryModule = angular.module('galleryModule', [ 'ngResource' ]);
 
-// Controller List
-galleryModule.controller('galleryCtrl', [ '$scope', 'Gallery',
-		function($scope, Gallery) {
-			var listGallery = $scope.listGallery = Gallery.query();
-
-		} // END CONTROLLER
-]);
-
-// Controller Show gallery
-galleryModule.controller('showGalleryCtrl', function($scope, $routeParams,
-		Gallery) {
-	var init = function() {
-		$scope.pictureToShow = null;
-		$scope.showModal = false;
-
-		// GET gallery et create slides
-		$scope.gallery = Gallery.get({
-			id : $routeParams.idGallery
-		}, function(res) {
-			$scope.slides = res.pictures.map(function(item) {
-				return {
-					id : item.id,
-					title : item.title,
-					active : false,
-					loaded : false
-				};
-			});
+	// *******************************************
+	// Resource Gallery
+	// *******************************************
+	galleryModule.factory('Gallery', function($resource) {
+		return $resource('api/gallery/:id', {
+			id : '@id'
 		});
-	}
+	});
 
-	/** Selection d'une image */
-	$scope.selectPicture = function(picture) {
-		$scope.pictureToShow = picture;
-		$scope.showModal = picture != null;
+	// *******************************************
+	// Controller List
+	// *******************************************
+	galleryModule.controller('galleryCtrl', function($scope, Gallery) {
+		$scope.listGallery = Gallery.query();
+	} // END CONTROLLER
+	);
 
-		if ($scope.showModal)
-			$scope.slides.find(function(item) {
-				return item.id == picture.id
-			}).active = true;
-	};
+	// *******************************************
+	// Controller Show gallery
+	// *******************************************
+	galleryModule.controller('showGalleryCtrl', function($scope, $routeParams,
+			Gallery) {
+		/** Init function */
+		var init = function() {
+			$scope.showModal = false;
 
-	/** Determine l'url a utiliser pour un slide/img */
-	$scope.getPictureUrl = function(slide) {
-		if (slide.active && !slide.loaded) {
-			slide.loaded = true;
+			// GET gallery et create slides
+			$scope.gallery = Gallery.get({
+				id : $routeParams.idGallery
+			}, function(res) {
+				$scope.slides = res.pictures.map(function(item) {
+					return {
+						id : item.id,
+						title : item.title,
+						active : false,
+						loaded : false
+					};
+				});
+			});
 		}
 
-		// Retourne soit la vrai url soit celle de chargement
-		return slide.loaded ? 'api/gallery/' + $scope.gallery.id + '/picture/'
-				+ slide.title : 'img/loading-gallery.gif';
-	};
+		/** Selection d'une image */
+		$scope.selectPicture = function(picture) {
+			if (picture != null) {
+				// Active le bon slide
+				$scope.slides.find(function(item) {
+					return item.id == picture.id
+				}).active = true;
 
-	// INIT
-	init();
-} // END CONTROLLER
-);
+				$scope.showModal = true;
+			} else {
+				$scope.showModal = false;
+			}
+		};
+
+		/** Determine l'url a utiliser pour l'img d'un slide */
+		$scope.getPictureUrl = function(slide) {
+			if (slide.active && !slide.loaded) {
+				slide.loaded = true;
+			}
+
+			// Retourne soit la vrai url soit celle de chargement
+			return slide.loaded ? 'api/gallery/' + $scope.gallery.id
+					+ '/picture/' + slide.title : 'img/loading-gallery.gif';
+		};
+
+		// INIT
+		init();
+	}); // END CONTROLLER
+
+})();
