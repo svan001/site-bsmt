14ac32780a0f915060e50ef040134fe3d38cc471
stephane <stephane.gronowski@gmai.com>
2016-04-04 20:36:37 +0200

    FIX : pb d'encodage en prod suite a reset de la bdd flyway... clean finalement le fichier d'init et en profite pour éviter le pb de la colonne is_postulant #17 et supprimant la colonne du fichier (remove aussi la version 3_0_0 au passage)

--- src/main/resources/db/migration/V3_0_0__16_04_04_pn_init_v_1_0.sql
+++
@@ -1,5 +0,0 @@
-/**
- * Script de correction du schema importer de la prod pour le passage a flyway : contenait une colonne supplémentaire qui bloque les ajouts de membres
- */
-
-ALTER TABLE team_member DROP COLUMN is_postulant;
